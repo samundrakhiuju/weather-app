0aece67bb5e9ed8d497c9d93a6d6f3fd4e1a54a7
samundra-portfolio <samundrakhiuju44@gmail.com>
2026-04-15 12:24:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const apiKey = '7c44eeb2376838dde5ebb8f6b67aac92';
+
+function getWeather() {
+  const city = document.getElementById('city-input').value;
+
+  if (city === '') {
+    document.getElementById('error-msg').innerText = 'Please enter a city name.';
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.cod === '404') {
+        document.getElementById('error-msg').innerText = 'City not found. Try again.';
+        return;
+      }
+
+      document.getElementById('error-msg').innerText = '';
+      document.getElementById('city-name').innerText = data.name;
+      document.getElementById('temperature').innerText = data.main.temp + '°C';
+      document.getElementById('description').innerText = data.weather[0].description;
+      document.getElementById('humidity').innerText = 'Humidity: ' + data.main.humidity + '%';
+    })
+    .catch(error => {
+      document.getElementById('error-msg').innerText = 'Something went wrong. Try again.';
+    });
+}
